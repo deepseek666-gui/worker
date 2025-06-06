64d527ed821365f80d32f3098f54abefd515d5fc
deepseek666-gui <deepseek380@gmail.com>
2025-06-06 06:10:07 +0530

    Update keep-alive.sh

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# 1. Simulate web service (port 10000)
+# Simulate web service
 nohup python3 -m http.server 10000 >/dev/null 2>&1 &
 
-# 2. Loop SSH command every 5 sec
+# Start an infinite loop to run a command every 5 sec
 while true; do
-  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 4tguucEERxtEDFJfvR9yRFL4s@sgp1.tmate.io "ls"
+  # Connect and run 'ls' inside a tmux-compatible session
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 4tguucEERxtEDFJfvR9yRFL4s@sgp1.tmate.io << EOF
+    export TERM=xterm
+    bash -c 'ls && echo "---"'
+EOF
+
   sleep 5
 done
