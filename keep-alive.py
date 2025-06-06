6117cd94827f774394d72d3acba3c0b0000ee7c5
deepseek666-gui <deepseek380@gmail.com>
2025-06-06 05:33:55 +0530

    Update keep-alive.py

--- keep-alive.py
+++ keep-alive.py
@@ -4,24 +4,25 @@ import time
 SSH_HOST = "sgp1.tmate.io"
 SSH_PORT = 22
 SSH_USER = "4tguucEERxtEDFJfvR9yRFL4s"
-SSH_PASS = None  # if password needed
+SSH_PASS = None  # agar password hai to use karo, nahi to None
 
 def keep_alive_ssh():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    # Disable compression explicitly here:
-    ssh.connect(
-        SSH_HOST,
-        port=SSH_PORT,
-        username=SSH_USER,
-        password=SSH_PASS,
-        compress=False
-    )
-
+    # Create Transport separately to tweak compression
+    transport = paramiko.Transport((SSH_HOST, SSH_PORT))
+    
+    # Disable all compression methods:
+    transport._preferred_compression = ('none',)  # force 'none' compression
+    
+    transport.connect(username=SSH_USER, password=SSH_PASS)
+    
+    ssh._transport = transport
+    
     try:
         while True:
-            ssh.exec_command("echo alive")
+            stdin, stdout, stderr = ssh.exec_command('echo alive')
             time.sleep(10)
     except Exception as e:
         print("Error:", e)
